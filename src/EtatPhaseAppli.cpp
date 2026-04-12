3207c5ed5e000c46dec75413c9174277e33168f1
f-rolland <felix.rolland@polymtl.ca>
2026-04-12 18:03:22 -0400

    petit debut de programme

---
+++ src/EtatPhaseAppli.cpp
@@ -0,0 +1,104 @@
+/// EtatPhaseAppli.cpp
+// (C) 2026 maplefoxo
+// Purpose : 
+//
+#include "EtatPhaseAppli.h"
+
+void EtatPhaseCommandeIntitiale::AjouterYogourt(std::string type)
+{}
+
+void EtatPhaseCommandeIntitiale::SelectionnerYogourt(unsigned index)
+{}
+
+void EtatPhaseCommandeIntitiale::AjouterGarniture()
+{}
+
+void EtatPhaseCommandeIntitiale::Annuler()
+{}
+
+void EtatPhaseCommandeIntitiale::Reappliquer()
+{}
+
+void EtatPhaseCommandeIntitiale::TerminerCommande()
+{}
+
+void EtatPhaseCommandeIntitiale::PreparerCommande()
+{}
+
+void EtatPhaseCommandeIntitiale::ChangerModePaiment(std::string mode)
+{}
+
+void EtatPhaseCommandeIntitiale::Payer(StrategiePaiement & strat)
+{}
+
+void EtatPhaseCommandeIntitiale::VoirTotal()
+{}
+
+void EtatPhaseCommandeIntitiale::VoirStock()
+{}
+
+void EtatPhaseCommandePreparation::AjouterYogourt(std::string type)
+{}
+
+void EtatPhaseCommandePreparation::SelectionnerYogourt(unsigned index)
+{}
+
+void EtatPhaseCommandePreparation::AjouterGarniture()
+{}
+
+void EtatPhaseCommandePreparation::Annuler()
+{}
+
+void EtatPhaseCommandePreparation::Reappliquer()
+{}
+
+void EtatPhaseCommandePreparation::TerminerCommande()
+{}
+
+void EtatPhaseCommandePreparation::PreparerCommande()
+{}
+
+void EtatPhaseCommandePreparation::ChangerModePaiment(std::string mode)
+{}
+
+void EtatPhaseCommandePreparation::Payer(StrategiePaiement & strat)
+{}
+
+void EtatPhaseCommandePreparation::VoirTotal()
+{}
+
+void EtatPhaseCommandePreparation::VoirStock()
+{}
+
+void EtatPhaseCommandeTerminee::AjouterYogourt(std::string type)
+{}
+
+void EtatPhaseCommandeTerminee::SelectionnerYogourt(unsigned index)
+{}
+
+void EtatPhaseCommandeTerminee::AjouterGarniture()
+{}
+
+void EtatPhaseCommandeTerminee::Annuler()
+{}
+
+void EtatPhaseCommandeTerminee::Reappliquer()
+{}
+
+void EtatPhaseCommandeTerminee::TerminerCommande()
+{}
+
+void EtatPhaseCommandeTerminee::PreparerCommande()
+{}
+
+void EtatPhaseCommandeTerminee::ChangerModePaiment(std::string mode)
+{}
+
+void EtatPhaseCommandeTerminee::Payer(StrategiePaiement & strat)
+{}
+
+void EtatPhaseCommandeTerminee::VoirTotal()
+{}
+
+void EtatPhaseCommandeTerminee::VoirStock()
+{}
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
+#include "pch.h"
+
 #include <iostream>
-#include "pattern1/Pattern1.h"
-#include "pattern2/Pattern2.h"
-#include "ui/ConsoleColors.h"
 
-int main()
-{
-    Pattern1 p1(20);
-    Pattern2 p2(50);
 
-    std::cout << ConsoleColor::yellow << "Hello World!" << ConsoleColor::reset << std::endl;
-    std::cout << ConsoleColor::cyan << "Je suis Pattern1 :  " << p1.getValue() << ConsoleColor::reset << std::endl;
-    std::cout << ConsoleColor::cyan << "Je suis Pattern2 :  " << p2.getValue() << ConsoleColor::reset << std::endl;
-    std::cout << ConsoleColor::magenta << "Bon TP a tous :) " << ConsoleColor::reset << std::endl;
+#include "ui/UIManager.h"
 
+int main() {
+    UIMessage a{"aafsgagsd"};
+
+    UIManager::printf(a);
+    UIManager::printf("\ngagaggoogoo%i", Couleurs::CYAN, 69);
+    UIManager::cls();
     return 0;
 };
