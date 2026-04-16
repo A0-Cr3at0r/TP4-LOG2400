0189b8a0c7180256fcd6049efcf046e31b1d044e
f-rolland <felix.rolland@polymtl.ca>
2026-04-16 02:40:55 -0400

    inv push

--- src/Inventaire.h
+++ src/Inventaire.h
@@ -1,19 +1,37 @@
-#include "GarnitureInventaire.h"
+/// Inventaire.h
+// (C) 2026 maplefoxo
+// Purpose : 
+//
+
+#pragma once
+
+#include <pch.h>
+
+#include "Garniture.h"
+#include "Yogourt.h"
 #include <vector>
 #include <memory>
 
-class Inventaire
-{
-
+class Inventaire { // patron singleton, observateur
 public:
-    Inventaire();
-    ~Inventaire();
 
-    Garniture retirerGarniture(typeGarniture type);
-    void ajouterGarniture(typeGarniture type);
+	static Inventaire& get();
 
+
+	unsigned int	getNumGarnitures() const;
+	weak_ptr<Garniture>&	getGarniture(unsigned int index);
+	void enregistrerGarniture(GarnitureRegistre&&);
+
+	unsigned int	getNumYogourts() const;
+	weak_ptr<Garniture>&	getYogourt(unsigned int index);
+	void enregistrerGarniture(Yogourt&&);
 private:
-    std::vector<std::unique_ptr<GarnitureInventaire>> inventaires_;
+	// singleton, enforce inst unique
+	Inventaire();
+	Inventaire(const Inventaire&) = delete;
+	Inventaire& operator=(const Inventaire&) = delete;
+
+
+	const vector<unique_ptr<Yogourt>>& yogourts;
+	const vector<unique_ptr<GarnitureRegistre>>& garnitures;
 };
-
-
